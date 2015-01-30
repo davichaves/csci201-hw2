51fdb5efcded1ce7a4dcb02b9299632055753694
Davi Chaves <dchaves@usc.edu>
2015-01-29 16:08:20 -0800

    go to TA

--- src/dchaves_CSCI201_Assignment2/ReadFile.java
+++ src/dchaves_CSCI201_Assignment2/ReadFile.java
@@ -1,7 +1,41 @@
 package dchaves_CSCI201_Assignment2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+//import java.util.Scanner;
+
 public class ReadFile {
-	public ReadFile(String filename){
-		
+	private static String [] lines = new String[100];
+//	private static Scanner s; 
+//	private static String fileName = new String();
+	public static void readFile(String fileName) throws IOException, FileNotFoundException{
+        FileReader fileReader = new FileReader(fileName);
+        BufferedReader reader = new BufferedReader(fileReader);
+        int counter = 0;
+        while (true) {
+            lines[counter] = reader.readLine();
+            if (lines[counter] == null) {
+                break;
+            }
+            counter++;
+        }
+        reader.close();
+        fileReader.close();
+	}
+	public static void main(String [] args){
+		//System.out.print("Which file?");
+        //fileName = s.next();
+        try {
+			readFile("input.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+		} catch (IOException e) {
+			System.out.println("Error reading the file!");
+		}
+        for(int i = 0; i < 10; i++){
+        	System.out.println(lines[i]);
+        }
 	}
 }
--- src/dchaves_CSCI201_Assignment2/Test.java
+++
@@ -1,7 +0,0 @@
-package dchaves_CSCI201_Assignment2;
-
-public class Test {
-	public static void main(String [] args){
-		System.out.println("testing!");
-	}
-}
